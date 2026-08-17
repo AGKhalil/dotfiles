cd708753dc810db5b1d88d6cc2c2cbec313643e1
AGKhalil <elghazalykhalil@gmail.com>
2026-08-17 08:51:47 +0000

    minor change

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -2,6 +2,10 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
+  init = function()
+    vim.g.mkdp_port = "8585"
+    vim.g.mkdp_echo_preview_url = 1
+  end,
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
